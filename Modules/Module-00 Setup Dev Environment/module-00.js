0074a81226142d7dcea9f9847894b1a5bbf0cc2e
Michael Lopez <michael.lopez@easycode-it.com>
2022-07-07 09:57:42 +0200

    Added Header

--- Modules/Module-00 Setup Dev Environment/module-00.js
+++ Modules/Module-00 Setup Dev Environment/module-00.js
@@ -1,2 +1,13 @@
+//************************************************************************* */
+//
+// Module:      01
+// Title:       Variables
+// Guide Link:  https://michu44.github.io/Node.js-101/
+// Author:      Michael Lopez
+// Publisher:   EasyAcademy
+// Website:     https://www.easyacademy.swiss/Node-JS-Guide
+//
+//************************************************************************* */
+
 // console.log will print out "test" in the console
 console.log("test");
---
+++ Modules/Module-01 Variables/module-01.js
@@ -0,0 +1,31 @@
+//************************************************************************* */
+//
+// Module:      01
+// Title:       Variables
+// Guide Link:  https://michu44.github.io/Node.js-101/
+// Author:      Michael Lopez
+// Publisher:   EasyAcademy
+// Website:     https://www.easyacademy.swiss/Node-JS-Guide
+//
+//************************************************************************* */
+
+// string
+var testvariabel = "test";
+
+// assignement
+var neueVar = testvariabel;
+
+// integer
+var intVar = 3;
+
+// boolean
+var boolVar = true;
+
+// null
+var nullVar = null;
+
+// float
+var floatVar = 3.6;
+
+// constant integer - can not be reassigned
+const intConst = 8;
