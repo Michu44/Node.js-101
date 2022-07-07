1cceca72440099a81f1a1716c5a825d10c469776
Michael Lopez <michael.lopez@easycode-it.com>
2022-07-07 11:39:28 +0200

    added operators

---
+++ Modules/Module-02 Operators/module-02.js
@@ -0,0 +1,39 @@
+//************************************************************************* */
+//
+// Module:      03
+// Title:       Operators
+// Guide Link:  https://michu44.github.io/Node.js-101/
+// Author:      Michael Lopez
+// Publisher:   EasyAcademy
+// Website:     https://www.easyacademy.swiss/Node-JS-Guide
+//
+//************************************************************************* */
+
+// Operators
+// Equals
+console.log(1 == 1); //true
+
+// Not Equals
+console.log(1 != 1); //false
+
+// smaller as
+console.log(0 < 1); //true
+
+// biger as
+console.log(0 > 1); //false
+
+// smaller or equals
+console.log(0 <= 1); // true
+console.log(1 <= 1); // true
+console.log(2 <= 1); // false
+
+
+console.log(0 >= 1); // false
+console.log(1 >= 1); // true
+console.log(2 >= 1); // true
+
+
+// Equality
+console.log(1 == "1"); //true
+// Strict Equality
+console.log(1 === "1"); //false
